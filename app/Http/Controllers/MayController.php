dbe6d0b5a1dc1dd696bcc97afb1abfe4d541ebe1
kunthid <ktd.homnan@gmail.com>
2017-02-09 09:50:46 +0700

    first commit

---
+++ app/Http/Controllers/MayController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MayController extends Controller
+{
+  function getYear(){
+
+  $byear = 1994;
+  $abc = $byear + 599999999;
+  echo decbin($abc);
+}
+
+  function getName($id)
+  {
+    $customers =
+    [
+      ["Name" => 'Madamm' , "Surname" => "Ktd"],
+      ["Name" => 'Patty' , "Surname" => "Story"]
+    ];
+    return $customers[$id-1];
+  }
+}
